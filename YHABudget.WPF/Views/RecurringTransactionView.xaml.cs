7c648d40203ff340c3f7ab11d0a1232ab8d4ebd0
Xergon <christoffer.isenberg@gmail.com>
2025-11-28 15:54:28 +0100

    Implement MainWindow UI with navigation and OverviewView
    - Create Colors.xaml with income/expense color scheme
    - Create Styles.xaml with navigation buttons and card styles
    - Update MainWindow with navigation bar and ContentControl
    - Add DataTemplates to map ViewModels to Views
    - Implement OverviewView with income/expense breakdown display
    - Create placeholder views for Transactions, Recurring, Settings
    - UI fully functional with data binding to OverviewViewModel

---
+++ YHABudget.WPF/Views/RecurringTransactionView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace YHABudget.WPF.Views;
+
+public partial class RecurringTransactionView : UserControl
+{
+    public RecurringTransactionView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ YHABudget.WPF/Views/TransactionView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace YHABudget.WPF.Views;
+
+public partial class TransactionView : UserControl
+{
+    public TransactionView()
+    {
+        InitializeComponent();
+    }
+}
