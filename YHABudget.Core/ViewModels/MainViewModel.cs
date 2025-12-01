fa7e1406e25dbf679c0579516c31377f714c7ed1
Xergon <christoffer.isenberg@gmail.com>
2025-12-01 11:46:34 +0100

    Add ViewModel caching to improve performance
    - Implement lazy initialization pattern with null-coalescing assignment
    - Cache OverviewViewModel, TransactionViewModel, RecurringTransactionViewModel, and SettingsViewModel
    - Prevent unnecessary ViewModel recreation on navigation
    - Fix flickering issue when opening ComboBox in TransactionView

--- YHABudget.Core/ViewModels/MainViewModel.cs
+++ YHABudget.Core/ViewModels/MainViewModel.cs
@@ -1,6 +1,7 @@
 using System.Windows.Input;
 using YHABudget.Core.Commands;
 using YHABudget.Core.MVVM;
+using YHABudget.Core.Services;
 using YHABudget.Data.Services;
 
 namespace YHABudget.Core.ViewModels;
@@ -10,27 +11,33 @@ public class MainViewModel : ViewModelBase
     private readonly ITransactionService _transactionService;
     private readonly ICategoryService _categoryService;
     private readonly ICalculationService _calculationService;
+    private readonly IDialogService _dialogService;
     private ViewModelBase? _currentViewModel;
+    
+    private OverviewViewModel? _overviewViewModel;
+    private TransactionViewModel? _transactionViewModel;
+    private RecurringTransactionViewModel? _recurringTransactionViewModel;
+    private SettingsViewModel? _settingsViewModel;
 
     public MainViewModel(
         ITransactionService transactionService, 
         ICategoryService categoryService,
-        ICalculationService calculationService)
+        ICalculationService calculationService,
+        IDialogService dialogService)
     {
         _transactionService = transactionService;
         _categoryService = categoryService;
         _calculationService = calculationService;
+        _dialogService = dialogService;
         
-        NavigateToOverviewCommand = new RelayCommand(() => CurrentViewModel = new OverviewViewModel(_transactionService, _calculationService));
-        NavigateToTransactionsCommand = new RelayCommand(() => CurrentViewModel = new TransactionViewModel(_transactionService, _categoryService));
-        NavigateToRecurringCommand = new RelayCommand(() => CurrentViewModel = new RecurringTransactionViewModel());
-        NavigateToSettingsCommand = new RelayCommand(() => CurrentViewModel = new SettingsViewModel());
+        NavigateToOverviewCommand = new RelayCommand(() => CurrentViewModel = GetOverviewViewModel());
+        NavigateToTransactionsCommand = new RelayCommand(() => CurrentViewModel = GetTransactionViewModel());
+        NavigateToRecurringCommand = new RelayCommand(() => CurrentViewModel = GetRecurringTransactionViewModel());
+        NavigateToSettingsCommand = new RelayCommand(() => CurrentViewModel = GetSettingsViewModel());
 
         // Start with Overview
-        CurrentViewModel = new OverviewViewModel(_transactionService, _calculationService);
-    }
-
-    public ViewModelBase? CurrentViewModel
+        CurrentViewModel = GetOverviewViewModel();
+    }    public ViewModelBase? CurrentViewModel
     {
         get => _currentViewModel;
         set => SetProperty(ref _currentViewModel, value);
@@ -40,4 +47,24 @@ public class MainViewModel : ViewModelBase
     public ICommand NavigateToTransactionsCommand { get; }
     public ICommand NavigateToRecurringCommand { get; }
     public ICommand NavigateToSettingsCommand { get; }
+    
+    private OverviewViewModel GetOverviewViewModel()
+    {
+        return _overviewViewModel ??= new OverviewViewModel(_transactionService, _calculationService);
+    }
+    
+    private TransactionViewModel GetTransactionViewModel()
+    {
+        return _transactionViewModel ??= new TransactionViewModel(_transactionService, _categoryService, _dialogService);
+    }
+    
+    private RecurringTransactionViewModel GetRecurringTransactionViewModel()
+    {
+        return _recurringTransactionViewModel ??= new RecurringTransactionViewModel();
+    }
+    
+    private SettingsViewModel GetSettingsViewModel()
+    {
+        return _settingsViewModel ??= new SettingsViewModel();
+    }
 }
